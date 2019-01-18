3ccd17df76595a911aa8a129f3a6ea402cc95096
mermadly <34113249+mermadly@users.noreply.github.com>
2019-01-18 12:16:57 -0300

    For primer intento

---
+++ practica.js
@@ -0,0 +1,36 @@
+// var userId = 10;
+
+// while (userId < 10) {
+//     console.log (userId);
+//     userId = userId + 1;
+
+// }
+
+// var texto = '';
+// var i = 0;
+
+// do {
+//  texto = 'El número de vuelta es: ' + i + '. ';
+// console.log (texto);
+//  i++;
+// } while (i < 5);
+
+
+var i;
+
+for (i = 0; i < 5; i++) { 
+    console.log ('Hola ' + i);
+}
+
+var listaNombres =  ['Mica', 'Vicki', 'Cata', 'Mar', 'Aldi'];
+
+var humanaDeAda =  listaNombres  [4];
+
+console.log (humanaDeAda);
+
+console.log(listaNombres.length);
+
+for (var i = 0; i < listaNombres.length; i++) {
+    // instrucción que quiero repetir
+    console.log (i+1, listaNombres [i] ) ;
+}
